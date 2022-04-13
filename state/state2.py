a676da73f2e0110ebec57fbc207f5e07663d36ac
Lopes, Maycon B <lopes.maycon@gmail.com>
2022-04-13 07:43:04 -0300

    Refatorado o código para que não seja preciso chamar transitar_para() após cada instanciação de um novo estado. Agora, ao ser instanciado um novo estado, este próprio cuida de chamar transitar_para() direto pelo construtor.

--- state/state2.py
+++ state/state2.py
@@ -31,45 +31,45 @@ class Ligado(Estado):
         print("Ligando o rádio...")
         return self
 
+    def __init__(self):
+        self.transitar_para()
 
 class Desligado(Estado):
     def transitar_para(self):
         print("Desligando o rádio...")
         return self
 
+    def __init__(self):
+        self.transitar_para()
 
 class AumentoDoVolume(Estado):
     def transitar_para(self):
         print("Aumentando o volume em 10...")
         return self
 
+    def __init__(self):
+        self.transitar_para()
 
 class ReducaoDoVolume(Estado):
     def transitar_para(self):
         print("Reduzindo o volume em 10...")
         return self
 
+    def __init__(self):
+        self.transitar_para()
 
 Radio = EstacaoDeRadio()
 print(f"O estado atual do rádio é: {type(Radio.estado).__name__}")
 
-ON = Ligado()
-OFF = Desligado()
-
-MaisAlto = AumentoDoVolume()
-MaisBaixo = ReducaoDoVolume()
-
-Radio.estado = ON
-Radio.transitar_para()
-
+Radio.estado = Ligado()
 print(f"O estado atual do rádio é: {type(Radio.estado).__name__}")
 
 print("Aumentando o volume...")
-Radio.estado = MaisAlto
-Radio.transitar_para()
+Radio.estado = AumentoDoVolume()
+#Radio.transitar_para()
 print(f"O estado atual do rádio é: {type(Radio.estado).__name__}")
 
 print("Desligando o dispositivo...")
-Radio.estado = OFF
-Radio.transitar_para()
+Radio.estado = Desligado()
+#Radio.transitar_para()
 print(f"O estado atual do rádio é: {type(Radio.estado).__name__}")
