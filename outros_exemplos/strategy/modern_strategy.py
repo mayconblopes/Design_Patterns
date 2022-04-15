f4fcf97e0b5647dc9bd3d3472b1927d644c8a3d5
Lopes, Maycon B <lopes.maycon@gmail.com>
2022-04-15 13:09:17 -0300

    Excluídos comentários desnecessários

--- outros_exemplos/strategy/modern_strategy.py
+++ outros_exemplos/strategy/modern_strategy.py
@@ -9,4 +9,15 @@ def iss_calc(venda: Venda) -> float:
     return venda.total_venda() * 0.02
 
 
+# --------------------------CÓDIGO CLIENTE ABAIXO-------------------------------
 
+venda = Venda("Caneta", 1.5, 56)
+icms = icms_calc(venda)
+iss = iss_calc(venda)
+
+print(f"O produto comprado foi {venda.produto}.\n"
+      f"A quantidade comprada foi {venda.quantidade}\n"
+      f"O total é de {venda.total_venda()}\n"
+      f"ICMS: {icms}\n"
+      f"ISS: {iss}\n"
+      f"Obrigado pela compra!")
