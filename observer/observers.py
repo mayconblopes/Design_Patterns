383cb947c1a77ed5e2ebc5daa7bb83f6b559373a
Lopes, Maycon B <lopes.maycon@gmail.com>
2022-04-15 07:32:41 -0300

    Aqui os observadores concretos que implementam a interface Observer são criados

---
+++ observer/observers.py
@@ -0,0 +1,15 @@
+from observer.observer_interface import Observer
+from observer.publicadora_interface import PublicadoraInterface
+from observer.sujeito1 import Sujeito1
+
+
+class Observer1(Observer):
+    def update(self, sujeito: PublicadoraInterface) -> None:
+        if sujeito.estado < 3:
+            print("\nObserver1 diz: Identifiquei um evento que me diz respeito!")
+
+
+class Observer2(Observer):
+    def update(self, sujeito: PublicadoraInterface) -> None:
+        if sujeito.estado == 0 or sujeito.estado >= 2:
+            print("\nObserver2 diz: Identifiquei um evento que me diz respeito!")
